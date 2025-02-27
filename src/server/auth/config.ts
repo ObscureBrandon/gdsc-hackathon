34a93b9c03a9fdc9096718326a743ec36cdae07f
ObscureBrandon <thegamingcop2005@gmail.com>
2025-02-28 01:02:40 +0200

    Make eslint work

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,6 +1,5 @@
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
 import GoogleProvider from "next-auth/providers/google";
 
 import { db } from "~/server/db";
@@ -40,7 +39,6 @@ declare module "next-auth" {
  */
 export const authConfig = {
   providers: [
-    DiscordProvider,
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
